74461e9273b5eea1b9e28a77963c32390043ee89
Onimi <onimi@ukr.net>
2018-09-23 10:18:08 +0300

    weather widget draft

--- src/app/weather-widget/weather-widget.component.ts
+++ src/app/weather-widget/weather-widget.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'weather-widget',
   templateUrl: './weather-widget.component.html',
-  styleUrls: ['./weather-widget.component.css']
+  styleUrls: ['./weather-widget.component.css'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class WeatherWidgetComponent implements OnInit {
   public name: string = 'Weather Widget';
